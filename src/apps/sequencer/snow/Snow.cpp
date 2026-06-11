931c058ef3a0633575d3f1ffda6ead5080f433d2
Matt K <4858543+mtklr@users.noreply.github.com>
2026-06-10 20:33:21 -0400

    add lo-res, pile, shapes options

--- src/apps/sequencer/snow/Snow.cpp
+++ src/apps/sequencer/snow/Snow.cpp
@@ -12,6 +12,19 @@
 #define MAX_SNOW 100
 #define INTENSITY 20
 
+// #define SNOW_LORES 1
+// #define SNOW_PILE 1
+// #define SNOW_SHAPES 1
+
+#ifdef SNOW_LORES
+#define FB_SCALE 2
+#else
+#define FB_SCALE 1
+#endif
+
+#define FB_WIDTH CONFIG_LCD_WIDTH / FB_SCALE
+#define FB_HEIGHT CONFIG_LCD_HEIGHT / FB_SCALE
+
 typedef struct {
     int size;
     uint8_t color;
@@ -22,6 +35,9 @@ typedef struct {
     float freq;
     float wobble;
     int falling;
+#ifdef SNOW_SHAPES
+    int shape;
+#endif
 } Snowflake;
 
 typedef struct {
@@ -30,26 +46,33 @@ typedef struct {
     Snowflake flake[MAX_SNOW];
 } Snowfall;
 
+#ifdef SNOW_PILE
 static const float temperature = -8.0f;
-static const int melt_threshold = (int) (-1.0f * temperature * (CONFIG_LCD_WIDTH * CONFIG_LCD_HEIGHT) / 70);
+static const int melt_threshold = (int) (-1.0f * temperature * (FB_WIDTH * FB_HEIGHT) / 70);
+#endif
 static const float Pi = 3.1415926536f;
 static const uint8_t flake_colors[7] = { 0x3, 0x5, 0x7, 0x9, 0xa, 0xc, 0xf };
 
 Snowfall snow;
 float wind;
 
-static uint8_t snow_fb[CONFIG_LCD_WIDTH * CONFIG_LCD_HEIGHT];
+#ifdef SNOW_PILE
+static uint8_t snow_fb[FB_WIDTH * FB_HEIGHT];
+#endif
 
 static void flake_init(Snowflake *flake, const int width) {
     flake->size = rand() % 2 + 1; // 1..2
     flake->color = flake_colors[rand() % 7];
     flake->x = RANDF(width);
     flake->y = 0.0f;
-    flake->speed = 0.3f + RANDF(1.2f);
+    flake->speed = (0.3f + RANDF(1.2f)) / FB_SCALE;
     flake->phase = RANDF(2.0f * Pi);
     flake->freq = RANDF(0.2f);
     flake->wobble = 0.5f + RANDF(2.5f);
     flake->falling = 1;
+#ifdef SNOW_SHAPES
+    flake->shape = rand() % 6;
+#endif
 }
 
 static Snowfall* snow_start(const int intensity, const int width) {
@@ -64,23 +87,24 @@ static Snowfall* snow_start(const int intensity, const int width) {
     return s;
 }
 
+#ifdef SNOW_PILE
 static int flake_blocked(Snowflake * flake, int x) {
     int y = (int) floorf(flake->y);
 
-    if (y + flake->size > CONFIG_LCD_HEIGHT - 1) {
-        flake->y = CONFIG_LCD_HEIGHT - flake->size;
+    if (y + flake->size > FB_HEIGHT - 1) {
+        flake->y = FB_HEIGHT - flake->size;
         return 1;
     }
 
     for (int fs = 1; fs <= flake->size; fs++) {
         // below not blank
-        if (snow_fb[(y + 1) * CONFIG_LCD_WIDTH + x] != 0) {
+        if (snow_fb[(y + 1) * FB_WIDTH + x] != 0) {
             int dir = rand() % 2 * -2 + 1; // -1 or 1
 
-            if (snow_fb[(y + fs) * CONFIG_LCD_WIDTH + (x + dir)] == 0) {
+            if (snow_fb[(y + fs) * FB_WIDTH + (x + dir)] == 0) {
                 flake->x += dir;
                 return 0;
-            } else if (snow_fb[(y + fs) * CONFIG_LCD_WIDTH + (x - dir)] == 0) {
+            } else if (snow_fb[(y + fs) * FB_WIDTH + (x - dir)] == 0) {
                 flake->x -= dir;
                 return 0;
             } else {
@@ -93,15 +117,16 @@ static int flake_blocked(Snowflake * flake, int x) {
 }
 
 static void melt_flakes() {
-    for (int x = 0; x < CONFIG_LCD_WIDTH; x++) {
-        for (int y = CONFIG_LCD_HEIGHT - 1; y > 0; y--) {
-            int index = y * CONFIG_LCD_WIDTH + x;
+    for (int x = 0; x < FB_WIDTH; x++) {
+        for (int y = FB_HEIGHT - 1; y > 0; y--) {
+            int index = y * FB_WIDTH + x;
 
-            // "drop" snow pile down 1 row
-            snow_fb[index] = snow_fb[(y - 1) * CONFIG_LCD_WIDTH + x];
+            // move snow pile down 1 row
+            snow_fb[index] = snow_fb[(y - 1) * FB_WIDTH + x];
         }
     }
 }
+#endif
 
 Snow::Snow() {
 }
@@ -119,9 +144,11 @@ void Snow::init() {
         wind = -wind;
     };
 
-    for (int i = 0; i < CONFIG_LCD_WIDTH * CONFIG_LCD_HEIGHT; i++) {
+#ifdef SNOW_PILE
+    for (int i = 0; i < FB_WIDTH * FB_HEIGHT; i++) {
         snow_fb[i] = 0;
     }
+#endif
 
     Snowfall *s = &snow;
     s = snow_start(INTENSITY, FB_WIDTH);
@@ -146,13 +173,14 @@ void Snow::draw(Canvas &canvas) {
         flake->x += wind;
 
         // wrap x from left to right
-        if (flake->x < 0) {
-            flake->x = CONFIG_LCD_WIDTH;
+        if (flake->x * 2 < 0) {
+            flake->x = FB_WIDTH;
         }
 
         // % _WIDTH = wrap x from right to left
-        int x = (int) floorf(flake->x + flake->wobble * sinf(flake->phase)) % CONFIG_LCD_WIDTH;
+        int x = (int) floorf(flake->x * FB_SCALE + flake->wobble * sinf(flake->phase)) % FB_WIDTH;
 
+#ifdef SNOW_PILE
         if (flake_blocked(flake, x) == 1) {
             flake->falling = 0;
 
@@ -160,37 +188,78 @@ void Snow::draw(Canvas &canvas) {
             if (flake->color > 0x5) { // skip darker flakes
                 for (int fx = 0; fx < flake->size; fx++) {
                     for (int fy = 0; fy < flake->size; fy++) {
-                        snow_fb[(int) (flake->y + fy) * CONFIG_LCD_WIDTH + (x + fx)] = rand() % 8 < 6 ? 0xf : 0xc;
+                        snow_fb[(int) (flake->y + fy) * FB_WIDTH + (x + fx)] = rand() % 8 < 6 ? 0xf : 0xc;
                     }
                 }
             }
         }
+#else
+        if (flake->y > FB_HEIGHT - 1) {
+            flake->falling = 0;
+        }
+#endif
 
         if (flake->falling) {
             canvas.setColor(flake->color);
-            canvas.drawRect(x, flake->y, flake->size, flake->size);
+
+#ifdef SNOW_SHAPES
+            switch (flake->shape) {
+                case 0:
+                    canvas.point(x * FB_SCALE, flake->y * FB_SCALE);
+                    break;
+                case 1:
+                    canvas.drawRect(x * FB_SCALE, flake->y * FB_SCALE, flake->size * FB_SCALE, flake->size * FB_SCALE);
+                    break;
+                case 2:
+                    canvas.fillRect(x * FB_SCALE, flake->y * FB_SCALE, flake->size * FB_SCALE, flake->size * FB_SCALE);
+                    break;
+                case 3:
+                    canvas.drawRect(x * FB_SCALE, flake->y * FB_SCALE + 1, 4, 2);
+                    canvas.drawRect(x * FB_SCALE + 1, flake->y * FB_SCALE, 2, 4);
+                    break;
+                case 4:
+                    canvas.hline(x * FB_SCALE + 1, flake->y * FB_SCALE, 2);
+                    canvas.hline(x * FB_SCALE + 1, flake->y * FB_SCALE + 3, 2);
+                    canvas.vline(x * FB_SCALE, flake->y * FB_SCALE + 1, 2);
+                    canvas.vline(x * FB_SCALE + 3, flake->y * FB_SCALE + 1, 2);
+                    break;
+                case 5:
+                    canvas.point(x * FB_SCALE, flake->y * FB_SCALE);
+                    canvas.point(x * FB_SCALE + 3, flake->y * FB_SCALE);
+                    canvas.fillRect(x * FB_SCALE + 1, flake->y * FB_SCALE + 1, 2, 2);
+                    canvas.point(x * FB_SCALE, flake->y * FB_SCALE + 3);
+                    canvas.point(x * FB_SCALE + 3, flake->y * FB_SCALE + 3);
+                    break;
+                default:
+                    break;
+            }
+#else
+            canvas.fillRect(x * FB_SCALE, flake->y * FB_SCALE, flake->size * FB_SCALE, flake->size * FB_SCALE);
+#endif
 
             flake->y += flake->speed;
             flake->phase += flake->freq;
         } else {
-            flake_init(flake, CONFIG_LCD_WIDTH);
+            flake_init(flake, FB_WIDTH);
         }
     }
 
+#ifdef SNOW_PILE
     // draw snow_fb
-    for (int x = 0; x < CONFIG_LCD_WIDTH; x++) {
-        for (int y = 0; y < CONFIG_LCD_HEIGHT; y++) {
-            if (snow_fb[y * CONFIG_LCD_WIDTH + x] == 0) {
+    for (int x = 0; x < FB_WIDTH; x++) {
+        for (int y = 0; y < FB_HEIGHT; y++) {
+            if (snow_fb[y * FB_WIDTH + x] == 0) {
                 continue;
             }
-            canvas.setColor(snow_fb[y * CONFIG_LCD_WIDTH + x]);
-            canvas.point(x, y);
+            canvas.setColor(snow_fb[y * FB_WIDTH + x]);
+            canvas.fillRect(x * FB_SCALE, y * FB_SCALE, FB_SCALE, FB_SCALE);
         }
     }
 
     if (rand() % melt_threshold == 0) {
         melt_flakes();
     }
+#endif
 
     if (os::ticks() > _delay + os::time::ms(5000)) {
         _delay = os::ticks();
