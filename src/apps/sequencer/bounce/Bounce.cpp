dfd498fa6c539c5cd6c019ee0ae997b5a5edada5
Matt K <4858543+mtklr@users.noreply.github.com>
2026-06-22 14:48:07 -0400

    lower max notes, cleanup

--- src/apps/sequencer/bounce/Bounce.cpp
+++ src/apps/sequencer/bounce/Bounce.cpp
@@ -1,5 +1,5 @@
-/* adapted from - https://github.com/godfreybrew/Ball-Simulation-in-C.git */
-/* inspiration - http://freetoolsassociation.com/detail_snapshot_Noise_Tracker_noisetracker_3.html */
+// adapted from - https://github.com/godfreybrew/Ball-Simulation-in-C.git
+// inspiration - http://freetoolsassociation.com/detail_snapshot_Noise_Tracker_noisetracker_3.html
 
 #include "Bounce.h"
 
@@ -9,51 +9,51 @@
 
 #define GRAVITY 0.4
 #define DAMPENING 0.7
-#define MAX_CIRCLES 20
-#define CIRCLE_RADIUS 4
-
-const int note_colors[7] = { 0x3, 0x5, 0x7, 0x9, 0xa, 0xc, 0xf };
-int num_circles;
+#define MAX_NOTES 12
+#define NOTE_RADIUS 4
 
 typedef struct {
     float x, y, radius, dx, dy;
     uint8_t color;
     bool flag;
-} Circle;
+} note;
 
-static Circle circles[MAX_CIRCLES];
+static const int note_colors[7] = { 0x3, 0x5, 0x7, 0x9, 0xa, 0xc, 0xf };
 
-static void step(Circle *circle, float dymax) {
-    circle->x += circle->dx;
-    circle->y += circle->dy;
-    circle->dy += GRAVITY;
+int num_notes;
+note notes[MAX_NOTES];
+
+static void step(note *note, float dymax) {
+    note->x += note->dx;
+    note->y += note->dy;
+    note->dy += GRAVITY;
 
     // collision with top
-    if (circle->y < 0) {
-        circle->y = 0;
-        circle->dy *= -1; // * DAMPENING;
+    if (note->y < 0) {
+        note->y = 0;
+        note->dy *= -1; // * DAMPENING;
     }
 
     // collision with bottom
-    if (circle->y + circle->radius > CONFIG_LCD_HEIGHT) {
-        circle->y = CONFIG_LCD_HEIGHT - circle->radius;
-        circle->dy *= -1; // * DAMPENING;
+    if (note->y + note->radius > CONFIG_LCD_HEIGHT) {
+        note->y = CONFIG_LCD_HEIGHT - note->radius;
+        note->dy *= -1; // * DAMPENING;
     }
 
     //collision with right
-    if (circle->x + circle->radius > CONFIG_LCD_WIDTH) {
-        circle->x = CONFIG_LCD_WIDTH - circle->radius;
-        circle->dx *= -1; // * DAMPENING;
+    if (note->x + note->radius > CONFIG_LCD_WIDTH) {
+        note->x = CONFIG_LCD_WIDTH - note->radius;
+        note->dx *= -1; // * DAMPENING;
     }
 
     //collision with left
-    if (circle->x < 0) {
-        circle->x = 0;
-        circle->dx *= -1; // * DAMPENING;
+    if (note->x < 0) {
+        note->x = 0;
+        note->dx *= -1; // * DAMPENING;
     }
 
-    if (circle->dy > 6) {
-        circle->dy = dymax;
+    if (note->dy > 6) {
+        note->dy = dymax;
     }
 }
 
@@ -65,28 +65,28 @@ void Bounce::init() {
 
     srand(time(NULL));
 
-    // single random value for each circle ...
+    // single random value for each note ...
     // int rx = rand() % 2 + 2;
     // int ry = rand() % 2 + 2;
 
-    for (int i = 0; i < MAX_CIRCLES; i++) {
-        circles[i].x = rand() % (CONFIG_LCD_WIDTH - 10) + 5;
-        circles[i].y = rand() % (CONFIG_LCD_HEIGHT - 10) + 5;
-        circles[i].radius = CIRCLE_RADIUS;
+    num_notes = rand() % MAX_NOTES + 1;
+
+    for (int i = 0; i < num_notes; i++) {
+        notes[i].x = rand() % (CONFIG_LCD_WIDTH - 10) + 5;
+        notes[i].y = rand() % (CONFIG_LCD_HEIGHT - 10) + 5;
+        notes[i].radius = NOTE_RADIUS;
 
         // ...
-        // circles[i].dx = rx;
-        // circles[i].dy = ry;
+        // notes[i].dx = rx;
+        // notes[i].dy = ry;
 
-        // ... or random per each circle
-        circles[i].dx = rand() % 2 + 3;
-        circles[i].dy = rand() % 2 + 3;
+        // ... or random per each note
+        notes[i].dx = rand() % 2 + 3;
+        notes[i].dy = rand() % 2 + 3;
 
-        circles[i].color = note_colors[rand() % 7];
-        circles[i].flag = rand() % 10 < 3 ? true : false;;
+        notes[i].color = note_colors[rand() % 7];
+        notes[i].flag = rand() % 10 < 3 ? true : false;
     }
-
-    num_circles = rand() % (MAX_CIRCLES / 2) + (MAX_CIRCLES / 2) + 1;
 }
 
 void Bounce::update(float dt) {
@@ -98,30 +98,30 @@ void Bounce::draw(Canvas &canvas) {
     canvas.setColor(0);
     canvas.fill();
 
-    for (int i = 0; i < num_circles; i++) {
-        Circle *circle = &circles[i];
+    for (int i = 0; i < num_notes; i++) {
+        note *note = &notes[i];
 
-        canvas.setColor(circle->color);
-        canvas.fillRect(circle->x, circle->y, circle->radius, circle->radius);
+        canvas.setColor(note->color);
+        canvas.fillRect(note->x, note->y, note->radius, note->radius);
 
         canvas.setBlendMode(BlendMode::Set);
         canvas.setColor(0);
 
-        canvas.point(circle->x, circle->y);
-        canvas.point(circle->x + (circle->radius - 1), circle->y);
-        canvas.point(circle->x, circle->y + (circle->radius - 1));
-        canvas.point(circle->x + (circle->radius - 1), circle->y + (circle->radius - 1));
+        canvas.point(note->x, note->y);
+        canvas.point(note->x + (note->radius - 1), note->y);
+        canvas.point(note->x, note->y + (note->radius - 1));
+        canvas.point(note->x + (note->radius - 1), note->y + (note->radius - 1));
 
         canvas.setBlendMode(BlendMode::Add);
-        canvas.setColor(circle->color);
+        canvas.setColor(note->color);
 
-        canvas.vline(circle->x + (circle->radius - 1), circle->y - (circle->radius + 2), circle->radius + 3);
+        canvas.vline(note->x + (note->radius - 1), note->y - (note->radius + 2), note->radius + 3);
 
-        if (circle->flag) {
-            canvas.hline(circle->x + circle->radius, circle->y - (circle->radius + 1), 1);
-            canvas.hline(circle->x + circle->radius, circle->y - circle->radius, 2);
+        if (note->flag) {
+            canvas.hline(note->x + note->radius, note->y - (note->radius + 1), 1);
+            canvas.hline(note->x + note->radius, note->y - note->radius, 2);
         }
 
-        step(circle, circle->dy);
+        step(note, note->dy);
     }
 }
