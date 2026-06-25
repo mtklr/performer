294e70dfe72243032bbbf604cd4c118bd2ca9f4b
Matt K <4858543+mtklr@users.noreply.github.com>
2026-06-24 23:10:05 -0400

    cleanup comments

--- src/apps/sequencer/stars/Stars.cpp
+++ src/apps/sequencer/stars/Stars.cpp
@@ -1,24 +1,23 @@
-/* Copyright (C) 2002 W.P. van Paassen - peter@paassen.tmfweb.nl
+// adapted from - https://demo-effects.sourceforge.net
 
-   This program is free software; you can redistribute it and/or modify it under
-   the terms of the GNU General Public License as published by the Free
-   Software Foundation; either version 2 of the License, or (at your
-   option) any later version.
+/*
+Copyright (C) 2002 W.P. van Paassen - peter@paassen.tmfweb.nl
 
-   This program is distributed in the hope that it will be useful, but WITHOUT
-   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-   for more details.
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your
+option) any later version.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; see the file COPYING.  If not, write to the Free
-   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
 
-/* note that the code has not been fully optimized */
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to the Free
+Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-/* From part of The Demo Effect Collection:
-   http://demo-effects.sourceforge.net
-   Converted to curses & edited by mtklr, May 2014
+note that the code has not been fully optimized
 */
 
 #include "Stars.h"
@@ -35,20 +34,20 @@ typedef struct {
   // uint8_t color;
 } STAR;
 
-static STAR stars[NUMBER_OF_STARS];
+STAR stars[NUMBER_OF_STARS];
 
 static void init_star(STAR* star, int i) {
-    /* randomly init stars, generate them around the center of the screen */
+    // randomly init stars, generate them around the center of the screen
     star->xpos = -10.0 + (20.0 * (rand() / (RAND_MAX + 1.0)));
     star->ypos = -10.0 + (20.0 * (rand() / (RAND_MAX + 1.0)));
 
-    star->xpos *= 3072.0; /* change viewpoint */
+    star->xpos *= 3072.0; // change viewpoint
     star->ypos *= 3072.0;
 
     star->zpos = i;
     star->speed = 2 + (int) (2.0 * (rand() / (RAND_MAX + 1.0)));
 
-    // star->color = i >> 2; /* the closer to the viewer the brighter */
+    // star->color = i >> 2; // the closer to the viewer the brighter
 }
 
 Stars::Stars() {
@@ -81,7 +80,7 @@ void Stars::draw(Canvas &canvas) {
         x = (stars[i].xpos / stars[i].zpos) + (CONFIG_LCD_WIDTH >> 1);
         y = (stars[i].ypos / stars[i].zpos) + (CONFIG_LCD_HEIGHT >> 1);
 
-        /* check if a star leaves the screen */
+        // check if a star leaves the screen
         if (x < 0 || x > CONFIG_LCD_WIDTH - 1 || y < 0 || y > CONFIG_LCD_HEIGHT - 1) {
             init_star(stars + i, i + 1);
             continue;
