7aecbbd828aa7505cca9faa9d5e50730a572d07c
Matt K <4858543+mtklr@users.noreply.github.com>
2026-06-27 10:26:51 -0400

    Revert "keep framebuf index above 0, cleanup"
    This reverts commit 884dce43ee061a646e112aa7a82d03c59f9e8cb1.
    
    Looks fine on simulator but not fine on the hardware.

--- src/apps/sequencer/fire/Fire.cpp
+++ src/apps/sequencer/fire/Fire.cpp
@@ -1,4 +1,4 @@
-// adapted from - https://fabiensanglard.net/doom_fire_psx/
+/* from https://fabiensanglard.net/doom_fire_psx/ */
 
 #include "Fire.h"
 
@@ -16,9 +16,8 @@
 
 #define FB_WIDTH CONFIG_LCD_WIDTH / FB_SCALE
 #define FB_HEIGHT CONFIG_LCD_HEIGHT / FB_SCALE
-#define FB_SIZE FB_WIDTH * FB_HEIGHT
 
-uint8_t fire_fb[FB_WIDTH * FB_HEIGHT];
+static uint8_t fire_fb[FB_WIDTH * FB_HEIGHT];
 
 Fire::Fire() {
 }
@@ -37,7 +36,7 @@ void Fire::init() {
     for (int i = 0; i < FB_WIDTH; i++) {
         // height - 2 offset avoids line at the top of display panel;
         // doesn't appear in simulator (??)
-        fire_fb[(FB_HEIGHT - 1) * FB_WIDTH + i] = 0xf; // FB_HEIGHT - 2 for display panel
+        fire_fb[(FB_HEIGHT - 1) * FB_WIDTH + i] = 0xf; // - 2
     }
 }
 
@@ -50,28 +49,27 @@ void Fire::draw(Canvas &canvas) {
     canvas.setColor(0);
     canvas.fill();
 
-    for (int y = 0; y < FB_HEIGHT - 0; y++) { // FB_HEIGHT - 1 for display panel
-        for (int x = 0; x < FB_WIDTH; x++) {
+    for (int x = 0; x < FB_WIDTH; x++) {
+        for (int y = 0; y < FB_HEIGHT - 0; y++) { // height - 1 for display panel
             int src = y * FB_WIDTH + x;
 
-            if (fire_fb[src] > 0) {
+            if (fire_fb[src] == 0) {
+                fire_fb[src - FB_WIDTH] = 0;
+            } else {
                 int rnd = rand() * 3 & 3;
                 int dst = src - rnd + 1;
 
-                // int d = src - FB_WIDTH > 0 ? src - FB_WIDTH : 0;
-                // fire_fb[d] = fire_fb[src] - 1; // stationary
-                // fire_fb[d] = fire_fb[src] - (rand() & 1); // movement
-
-                int d = dst - (FB_WIDTH / FB_SCALE) > 0 ? dst - (FB_WIDTH / FB_SCALE) : 0;
-                fire_fb[d] = fire_fb[src] - (rnd & 1); // as above with left/right movement
+                // fire_fb[src - FB_WIDTH] = fire_fb[src] - 1; // stationary
+                // fire_fb[src - FB_WIDTH] = fire_fb[src] - (rand() & 1); // movement
+                fire_fb[dst - (FB_WIDTH / FB_SCALE)] = fire_fb[src] - (rnd & 1); // as above with left/right movement
             }
 
             canvas.setColor(fire_fb[src]);
 
 #ifdef FIRE_LORES
-            // canvas.point(x * FB_SCALE, y * FB_SCALE);
-            // canvas.drawRect(x * FB_SCALE, y * FB_SCALE, 3, 3);
             canvas.drawRect(x * FB_SCALE, y * FB_SCALE, 2, 2);
+            // canvas.drawRect(x * FB_SCALE, y * FB_SCALE, 3, 3);
+            // canvas.point(x * FB_SCALE, y * FB_SCALE);
 #else
             canvas.point(x, y);
 #endif
